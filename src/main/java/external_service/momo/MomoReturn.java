26ec5c53452f376c0df5c61aa733020b03cfff2e
kleitusOleny <23130383@st.hcmuaf.edu.vn>
2026-05-24 23:08:15 +0700

    [Fix] - Sửa lỗi xoá hết giỏ hàng khi thanh dù chưa thanh toán thành công

--- src/main/java/external_service/momo/MomoReturn.java
+++ src/main/java/external_service/momo/MomoReturn.java
@@ -3,6 +3,7 @@ package external_service.momo;
 import dao.OrderDAO;
 import dao.PaymentDAO;
 import dao.ShipOrderDAO;
+import jakarta.servlet.http.HttpSession;
 import model.Order;
 import model.Payment;
 import model.ShipOrder;
@@ -117,6 +118,29 @@ public class MomoReturn extends HttpServlet {
                         shipOrder.setEstimatedDeliveryDate(
                                 new Timestamp(System.currentTimeMillis() + 3 * 24 * 60 * 60 * 1000));
                         shipOrderDAO.save(shipOrder);
+                    }
+                    HttpSession session = request.getSession(false);
+                    if (session != null) {
+                        String checkoutType = (String) session.getAttribute("checkoutType");
+                        if ("buyNow".equals(checkoutType)) {
+                            session.removeAttribute("buyNowCart");
+                        } else {
+                            session.removeAttribute("cart");
+                            dao.CartDAO cartDAO = new dao.CartDAO();
+                            cartDAO.clearCart(order.getUserId());
+                        }
+                        session.removeAttribute("pendingOrder");
+                        session.removeAttribute("checkoutType");
+                    }
+                }else{
+                    dao.OrderItemDAO orderItemDAO = new dao.OrderItemDAO();
+                    services.ProductService productService = new services.ProductService();
+                    java.util.List<model.OrderItem> items = orderItemDAO.getByOrderId(parsedOrderId);
+                    if (items != null) {
+                        for (model.OrderItem item : items) {
+                            int currentStock = productService.getQuantity(item.getProductId());
+                            productService.updateQuantity(item.getProductId(), currentStock + item.getQuantity());
+                        }
                     }
                 }
 
--- src/main/java/external_service/vnpay/VnpayReturn.java
+++ src/main/java/external_service/vnpay/VnpayReturn.java
@@ -119,6 +119,30 @@ public class VnpayReturn extends HttpServlet {
                                 new Timestamp(System.currentTimeMillis() + 3 * 24 * 60 * 60 * 1000)); // +3 days
                         shipOrderDAO.save(shipOrder);
                     }
+                    
+                    HttpSession session = request.getSession(false);
+                    if (session != null) {
+                        String checkoutType = (String) session.getAttribute("checkoutType");
+                        if ("buyNow".equals(checkoutType)) {
+                            session.removeAttribute("buyNowCart");
+                        } else {
+                            session.removeAttribute("cart");
+                            dao.CartDAO cartDAO = new dao.CartDAO();
+                            cartDAO.clearCart(order.getUserId());
+                        }
+                        session.removeAttribute("pendingOrder");
+                        session.removeAttribute("checkoutType");
+                    }
+                }else {
+                    dao.OrderItemDAO orderItemDAO = new dao.OrderItemDAO();
+                    services.ProductService productService = new services.ProductService();
+                    java.util.List<model.OrderItem> items = orderItemDAO.getByOrderId(Integer.parseInt(orderId));
+                    if (items != null) {
+                        for (model.OrderItem item : items) {
+                            int currentStock = productService.getQuantity(item.getProductId());
+                            productService.updateQuantity(item.getProductId(), currentStock + item.getQuantity());
+                        }
+                    }
                 }
 
                 request.setAttribute("transResult", transSuccess);
